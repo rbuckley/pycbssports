7a643a905ccb366d39602b7c40a0807f4edb1580
Robert Buckley <rbuckley30@gmail.com>
2013-09-20 14:15:08 -0700

    made the __init__.py file

---
+++ cbssports/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#
+# pycbssports - Python bindings for the CBSSports Developer API
+#
+#
+#
+
+import sys
+import requests
+import json
+
+VERSION = '0.001a'
+
+CBSSPORTS_URL = 'http://api.cbssports.com/fantasy'
